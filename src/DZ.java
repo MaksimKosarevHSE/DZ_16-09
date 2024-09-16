431ba1e7872a5374fcfa636dcf67743ac28c75e1
Max <maksimkosarev96@gmail.com>
2024-09-17 02:43:25 +0300

    Начало выполнения пункта b)

--- src/DZ.java
+++ src/DZ.java
@@ -6,10 +6,6 @@ public class DZ {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("c4 c5");
-        System.out.println(a - b);
-        System.out.println("c1");
-        System.out.println("c2");
-        System.out.println("c3");
+        
     }
 }
