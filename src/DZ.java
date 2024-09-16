d4cc64728c44f52aa7d8c7674e26d72058902e44
Max <maksimkosarev96@gmail.com>
2024-09-17 02:00:11 +0300

    add input

--- src/DZ.java
+++ src/DZ.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class DZ {
     public static void main(String[] args) {
-
+        System.out.println("Введите 2 целых числа");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
     }
 }
