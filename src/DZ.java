0ec00590c09de3c81db12d3a478913979920685e
Max <maksimkosarev96@gmail.com>
2024-09-17 02:25:07 +0300

    c1

--- src/DZ.java
+++ src/DZ.java
@@ -8,10 +8,5 @@ public class DZ {
         int b = sc.nextInt();
         System.out.println(a - b);
         System.out.println("c1");
-        System.out.println("c2");
-        System.out.println("c3");
-        System.out.println("c4");
-        System.out.println("c5");
-        System.out.println("placeholder");
     }
 }
